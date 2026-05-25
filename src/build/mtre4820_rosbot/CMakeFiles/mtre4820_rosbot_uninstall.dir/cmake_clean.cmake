92c19ffc682a8975031c7f2a4124724cac10fa88
Saam-Grami <saam.haghighat.grami@gmail.com>
2026-05-25 00:57:09 -0400

    cleanup

--- src/build/mtre4820_rosbot/CMakeFiles/mtre4820_rosbot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mtre4820_rosbot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mtre4820_rosbot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/build/mtre4820_rosbot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gramisaam/mtre4820-final-project/src/mtre4820_rosbot
-# Build directory: /home/gramisaam/mtre4820-final-project/src/build/mtre4820_rosbot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
